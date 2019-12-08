67511db54d2a714d30e9a0bb521ea3791648bbb2
Jiten Shahani <shahani.jiten@gmail.com>
2019-12-09 00:48:27 +0530

    bg colors updated

--- Source/index.js
+++ Source/index.js
@@ -1,26 +1,28 @@
 function setBackground() {
 	window.setTimeout("setbackground()", 5000); // 5000 milliseconds delay
-	let index = Math.round(Math.random() * 9);
-	let ColorValue = "FFFFFF"; // default color - white (index = 0)
+	let index = Math.round(Math.random() * 10);
+	let colorValue = "#636e72"; // default color - white (index = 0)
 
 	if (index == 1)
-		ColorValue = "81ecec"; //peach
+		ColorValue = "#55efc4"; // light green
 	if (index == 2)
-		ColorValue = "00cec9"; //violet
+		ColorValue = "#81ecec"; // faded poster
 	if (index == 3)
-		ColorValue = "74b9ff"; //lt blue
+		ColorValue = "#74b9ff"; // green daner tail
 	if (index == 4)
-		ColorValue = "0984e3"; //cyan
+		ColorValue = "#a29bfe"; // shy moment
 	if (index == 5)
-		ColorValue = "6c5ce7"; //tan
+		ColorValue = "#dfe6e9"; // city lights
 	if (index == 6)
-		ColorValue = "a29bfe"; //lt green
+		ColorValue = "#00cec9"; // Robin's egg blue
 	if (index == 7)
-		ColorValue = "55efc4"; //lt yellow
+		ColorValue = "#b2bec3"; // soothing breeze
 	if (index == 8)
-		ColorValue = "b2bec3"; //lt orange
+		ColorValue = "#fab1a0"; // first date
 	if (index == 9)
-		ColorValue = "636e72"; //lt grey
+		ColorValue = "#ffeaa7"; // sour lemon
+	if (index == 10)
+		ColorValue = "#D6A2E8"; // bright ube
 
-	document.getElementsByTagName("body")[0].style.backgroundColor = "#" + ColorValue;
+	document.getElementsByTagName("body")[0].style.backgroundColor = ColorValue;
 }

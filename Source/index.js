6610b232ea08f2eaac7e41dad9bdb231d4005d4d
Jiten Shahani <shahani.jiten@gmail.com>
2019-12-16 20:19:07 +0530

    Squashed a bug

--- Source/index.js
+++ Source/index.js
@@ -6,23 +6,23 @@ function setBackground() {
 	if (index == 1)
 		colorValue = "#55efc4"; // light green
 	if (index == 2)
-		ColorValue = "#81ecec"; // faded poster
+		colorValue = "#81ecec"; // faded poster
 	if (index == 3)
-		ColorValue = "#74b9ff"; // green daner tail
+		colorValue = "#74b9ff"; // green daner tail
 	if (index == 4)
-		ColorValue = "#a29bfe"; // shy moment
+		colorValue = "#a29bfe"; // shy moment
 	if (index == 5)
-		ColorValue = "#dfe6e9"; // city lights
+		colorValue = "#dfe6e9"; // city lights
 	if (index == 6)
-		ColorValue = "#00cec9"; // Robin's egg blue
+		colorValue = "#00cec9"; // Robin's egg blue
 	if (index == 7)
-		ColorValue = "#b2bec3"; // soothing breeze
+		colorValue = "#b2bec3"; // soothing breeze
 	if (index == 8)
-		ColorValue = "#fab1a0"; // first date
+		colorValue = "#fab1a0"; // first date
 	if (index == 9)
-		ColorValue = "#ffeaa7"; // sour lemon
+		colorValue = "#ffeaa7"; // sour lemon
 	if (index == 10)
-		ColorValue = "#D6A2E8"; // bright ube
+		colorValue = "#D6A2E8"; // bright ube
 
-	document.getElementsByTagName("body")[0].style.backgroundColor = ColorValue;
+	document.getElementsByTagName("body")[0].style.backgroundColor = colorValue;
 }

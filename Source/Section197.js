0f2056bf5bab0a9e21ebb13a5aec543dc9e5982f
Jiten Shahani <shahani.jiten@gmail.com>
2020-01-04 15:41:25 +0530

    Color Game Project Section 199

--- Source/Section197.js
+++ Source/Section197.js
@@ -1,11 +1,4 @@
-let colors = [
-	'rgb(255, 0, 0)',
-	'rgb(255, 255, 0)',
-	'rgb(0, 255, 0)',
-	'rgb(0, 255, 255)',
-	'rgb(0, 0, 255)',
-	'rgb(255, 0, 255)',
-];
+let colors = generateRandomColors(6);
 
 let pickedColor = pickColor();
 let bodyColor = document.body.style.backgroundColor;
@@ -14,6 +7,7 @@ let bodyColor = document.body.style.backgroundColor;
 let squares = document.querySelectorAll('.square');
 let colorDisplay = document.querySelector('#colorDisplay');
 let messageDisplay = document.querySelector('#message');
+let h1 = document.querySelector('h1');
 //#endregion
 
 colorDisplay.textContent = pickedColor;
@@ -30,6 +24,7 @@ for (let i = 0; i < squares.length; i++) {
 		if (clickedColor === pickedColor) {
 			messageDisplay.textContent = 'Correct!';
 			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
 		} else {
 			this.style.backgroundColor = bodyColor;
 			messageDisplay.textContent = 'Try Again';
@@ -55,4 +50,34 @@ function changeColors(color) {
 function pickColor() {
 	let randomNumber = Math.floor(Math.random() * colors.length);
 	return colors[randomNumber];
+}
+
+/**
+ * This function loops through n times and call randomColor() function to generate a color
+ * @param {number} num
+ */
+function generateRandomColors(num) {
+	// make an array
+	let arr = [];
+	// repeat num times
+	for (let i = 0; i < num; i++) {
+		// get random color and push into arr
+		arr.push(randomColor());
+	}
+	// return that array
+	return arr;
+}
+
+/**
+ * This function generates a random color
+ */
+function randomColor() {
+	// pick a 'red' between 0 - 255
+	let red = Math.floor(Math.random() * 256);
+	// pick a 'green' between 0 - 255
+	let green = Math.floor(Math.random() * 256);
+	// pick a 'blue' between 0 - 255
+	let blue = Math.floor(Math.random() * 256);
+
+	return `rgb(${red}, ${green}, ${blue})`;
 }
